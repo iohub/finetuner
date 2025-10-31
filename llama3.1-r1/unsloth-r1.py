6459e719ec83bd33bf0e43bb26375fdb23c3067e
iohub <rickyang.pro@gmail.com>
2025-10-31 21:43:07 +0800

    r1 on llama3.1

---
+++ llama3.1-r1/unsloth-r1.py
@@ -0,0 +1,32 @@
+
+from unsloth import FastLanguageModel
+import torch
+max_seq_length = 1024 # Can increase for longer reasoning traces
+lora_rank = 32 # Larger rank = smarter, but slower
+
+model_path = '/home/do/ssd/modelscope/hub/models/LLM-Research/Meta-Llama-3___1-8B-Instruct'
+
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name = model_path,
+    max_seq_length = max_seq_length,
+    load_in_4bit = True, # False for LoRA 16bit
+    fast_inference = True, # Enable vLLM fast inference
+    max_lora_rank = lora_rank,
+    enforce_eager=True,
+    gpu_memory_utilization = 0.9, # Reduce if out of memory
+)
+
+model = FastLanguageModel.get_peft_model(
+    model,
+    r = lora_rank, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+    target_modules = [
+        "q_proj", "k_proj", "v_proj", "o_proj",
+        "gate_proj", "up_proj", "down_proj",
+    ], # Remove QKVO if out of memory
+    lora_alpha = lora_rank,
+    use_gradient_checkpointing = "unsloth", # Enable long context finetuning
+    random_state = 3407,
+)
+
+
+
